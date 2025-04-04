94bb52fdd325905b9067f81d1ef4b1a3ef850874
unknown <mouhssinezhirou@gmail.com>
2025-04-04 20:31:27 +0000

    first commit3

--- src/app/menu/starters/components/Starters.tsx
+++ src/app/menu/starters/components/Starters.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import React from 'react';
+import { motion} from 'framer-motion';
 
 interface MenuItem {
   name: string;
